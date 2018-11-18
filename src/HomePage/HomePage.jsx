62597d959156680994d16a5354eb3381f304774b
alexlehner86 <xander_lehner@gmx.at>
2018-11-18 12:17:13 +0100

    experimenting with pdf parse

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { crawler } from 'crawler-request';
 import './HomePage.css';
 
 class HomePage extends Component {
@@ -11,8 +12,16 @@ class HomePage extends Component {
               <p className="infotext">Die Webseite bietet dir einen vereinfachten Zugang zum österreichischen Bundes- und Landesrecht sowie zur Rechtsprechung. Du kannst einzelne Suchergebnisse in eigenen Projekt-Ordnern speichern.</p>
           </div>
           <img src="./logo.svg" alt="" className="homePageLogo" />
+          <a href="#" onClick={this.parsePDF}>Test PDF Parse</a>
         </div>
     );
+  }
+
+  parsePDF() {
+    crawler("http://careers.stackoverflow.com/stack_overflow_careers.pdf").then(function(response){
+      // handle response
+      console.log(response.text.lenght);
+    });
   }
 }
 

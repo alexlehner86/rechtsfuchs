b4e77c467dd1e9862e1f6a1a33d96788acad8691
alexlehner86 <xander_lehner@gmx.at>
2018-12-15 00:16:37 +0100

    new version 1.4.0

--- src/InfoPage/InfoPage.jsx
+++ src/InfoPage/InfoPage.jsx
@@ -8,7 +8,7 @@ class InfoPage extends Component {
           <h1>Informationen zur Webseite</h1>
           <ul className="infoliste">
             <li><span className="makeBold">App-Name:</span> Rechtsfuchs</li>
-            <li><span className="makeBold">Version:</span> 1.3.8</li>
+            <li><span className="makeBold">Version:</span> 1.4.0</li>
             <li><span className="makeBold">Autor:</span> <a href="https://github.com/alexlehner86" target="_blank" rel="noopener noreferrer">Alexander Lehner</a> (Wien, Österreich)</li>
             <li><span className="makeBold">Warum diese Webseite?</span><br />
                 Der Rechtsfuchs bietet einen vereinfachten Zugang zum österreichischen Bundes- und Landesrecht sowie zur Rechtsprechung, mit Fokus auf das Finanzrecht (über Suchwort-Vorschläge). Wichtig war mir ein Responsive Design, um auch eine komfortable Nutzung auf Smartphones und Tablets zu ermöglichen. Die Webseite nutzt die APIs des österreichischen Rechtsinformationssystems des Bundes (<a href="https://www.ris.bka.gv.at/" target="_blank" rel="noopener noreferrer">RIS</a>). Suchergebnisse können in eigenen Projekt-Ordnern verwaltet werden.</li>
